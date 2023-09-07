4c40b7063e0fefa808714ddac9e91accee29d736
Kritagyabhagat <117289078+Kritagyabhagat@users.noreply.github.com>
2023-09-07 21:53:21 +0530

    Create Harmonic_series.java

---
+++ Harmonic_series.java
@@ -0,0 +1,10 @@
+//Harmonic_series=1+1/2+1/3+1/4+1/5+1/6+1/7+1/8+1/9+.......+/n
+class Harmonic_series
+{
+	public static void main(String[]arg)
+	{
+		double harmonic_series;
+		harmonic_series=1+1/2+1/3+1/4+1/5+1/6+1/7+1/8+1/9;
+		System.out.println("Harmonic series = "+harmonic_series);
+	}
+}
