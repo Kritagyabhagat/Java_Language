4cec8bd589ff89b0ee26f8813e15fe8f91671807
Kritagyabhagat <117289078+Kritagyabhagat@users.noreply.github.com>
2023-09-07 22:04:57 +0530

    Create Check_marks.java

---
+++ Check_marks.java
@@ -0,0 +1,31 @@
+class Check_marks
+{
+public static void main(String[]arg)
+{
+int a=63;
+if(a>80)
+{
+System.out.println("Honar");
+}
+else if(a>70)
+{
+System.out.println("1st Division");
+}
+else if(a>60)
+{
+System.out.println("2nd Division");
+}
+else if(a>50)
+{
+System.out.println("3rd Division");
+}
+else if(a>40)
+{
+System.out.println("4th Division");
+}
+else
+{
+System.out.println("Fail");
+}
+}
+}
